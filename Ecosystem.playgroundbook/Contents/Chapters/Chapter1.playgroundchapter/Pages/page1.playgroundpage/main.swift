d65fefcc1139ec013f222687121014e160e45e23
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-04-13 16:20:53 -0300

    ADD note at the end of the markup

--- Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page1.playgroundpage/main.swift
+++ Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page1.playgroundpage/main.swift
@@ -23,8 +23,9 @@ PlaygroundPage.current.setLiveView(SimulationViewController())
  ### What is a ecosystem and why should I even care?
  
    According to the professor of Biology Lana Magalhães, ecosystem is a set of living organisms that live and interact with each other and with the environment, constituting a stable, balanced and self-sufficient system.\
-    That being said, I bet you already saw one! In fact, **you live in a ecosystem!** It might be different from mine or from someone across your country, there's a plenty of them on the planet, which all of them have different sizes and locations. It's basically our home, but in a larger scale... I think you get the importance 😉\
-    I could talk here all day about this, but since I don't wan't to take too much of your time, let's focus on how land ecosystems works. We are gonna start talking about... **plants** 🌿
+    That being said, I bet you already saw one! In fact, **you live in a ecosystem!** It might be different from mine or from someone across your country, there's a plenty of them on the planet, which all of them have different sizes and locations. It's basically our home, but in a larger scale... I think you get the importance 😊 \
+    I could talk here all day about this, but since I don't want to take too much of your time, let's focus on how land ecosystems works. We are gonna start talking about... **plants** 🌿
+ - Note: All info here will be very summarized to be fast to read while also talking about important points. The objective here is to motivate you to search and learn more by your own 😉
     
  [Click here to advance](@next)
  */
