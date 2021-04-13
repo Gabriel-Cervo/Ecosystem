a5b3bea75dc2767e0907e0a89060e44f7abb451c
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-04-13 16:08:57 -0300

    Added introduction page text

--- Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page1.playgroundpage/main.swift
+++ Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page1.playgroundpage/main.swift
@@ -1,1 +1,30 @@
+//#-hidden-code
+import PlaygroundSupport
+import SpriteKit
 
+PlaygroundPage.current.setLiveView(SimulationViewController())
+//#-end-hidden-code
+
+/*:
+ ![Ecosystem Poster](cover.png)
+ 
+ # Welcome to Ecosystem!
+ 
+ - callout(João): Hey! I'm glad you're here! You wan't to know more about ecosystems? That's great!
+
++ callout(João): Shortly you will learn all everything about how a ecosystem works, from the importance of plants all the way to carnivores. We will build and *try* to simulate our own ecosystem too! But if things go wrong, I won't take responsability...
+ 
+ * callout(WWDC): 🙄
+ 
+ - callout(João): Come on, i promise it will be fun!
+ 
+ + callout(João): But first, you may be wondering...
+
+ ### What is a ecosystem and why should I even care?
+ 
+   According to Vanessa dos Santos, ecosystem is a set of communities that live in a specific place and interact with each other and with the environment, constituting a stable, balanced and self-sufficient system.\
+    That being said, I bet you already saw one! In fact, you live in a ecosystem! It might be different from mine or from someone across your country, there's a plenty of them on the planet, which all of them have different sizes and locations. It's basically our home, but in a larger scale... I think you get the importance 😉\
+    I could talk here all day about this, but since I don't wan't to take too much of your time, let's focus on how land ecosystems works. We are gonna start talking about... plants 🌿
+    
+ [Click here to advance](@next)
+ */
