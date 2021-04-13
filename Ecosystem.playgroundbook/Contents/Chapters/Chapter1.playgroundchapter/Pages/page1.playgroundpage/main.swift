8cf42d581dde520ae0814167bb6ebac66ff917dd
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-04-13 16:12:22 -0300

    Add some bold text

--- Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page1.playgroundpage/main.swift
+++ Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page1.playgroundpage/main.swift
@@ -23,8 +23,8 @@ PlaygroundPage.current.setLiveView(SimulationViewController())
  ### What is a ecosystem and why should I even care?
  
    According to Vanessa dos Santos, ecosystem is a set of communities that live in a specific place and interact with each other and with the environment, constituting a stable, balanced and self-sufficient system.\
-    That being said, I bet you already saw one! In fact, you live in a ecosystem! It might be different from mine or from someone across your country, there's a plenty of them on the planet, which all of them have different sizes and locations. It's basically our home, but in a larger scale... I think you get the importance 😉\
-    I could talk here all day about this, but since I don't wan't to take too much of your time, let's focus on how land ecosystems works. We are gonna start talking about... plants 🌿
+    That being said, I bet you already saw one! In fact, **you live in a ecosystem!** It might be different from mine or from someone across your country, there's a plenty of them on the planet, which all of them have different sizes and locations. It's basically our home, but in a larger scale... I think you get the importance 😉\
+    I could talk here all day about this, but since I don't wan't to take too much of your time, let's focus on how land ecosystems works. We are gonna start talking about... **plants** 🌿
     
  [Click here to advance](@next)
  */
