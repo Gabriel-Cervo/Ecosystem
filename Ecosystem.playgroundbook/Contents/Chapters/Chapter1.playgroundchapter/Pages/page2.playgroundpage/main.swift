80f9d676c491fbb6b9d14c0b5ce26d1c79588298
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-04-14 16:27:27 -0300

    Added more text to the plants page

--- Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page2.playgroundpage/main.swift
+++ Ecosystem.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/page2.playgroundpage/main.swift
@@ -4,6 +4,21 @@
     
 The plants are the biggest and one of the most diverse group of living beings on the planet. They are the basis of terrestrial ecosystems. Plants also provide us a substantial part of oxygen, which we need to survive, so it's important to take care of them!!
  
-- Callout(Did you know? 💡): Trees are a plant! Some people don't know this because they are big, but in fact, they are a very large plant!
+- Callout(Did you know? 💡):
+    Trees are a plant! Some people don't know this because they are big, but in fact, they are a very large plant!
+ 
+ ### Life Cycle
+ 
+ Plants starts life as a seed, which germinates and grows into a plant. The mature plant produces flowers, which are fertilised and produce seeds in a fruit or seedpod. The plant eventually dies, leaving seeds which will germinate to produce new plants, and the cycle repeats itself.
+ 
+ - Callout(Try it by yourself!):
+    Try cutting the branch of a healthy plant and put it in a suitable medium, such as moist soil. And then just wait for the plant to grow! But be aware to take good care of the plant, otherwise it will die and not grow 😓
+ 
+ ### Nutrition
+ 
+ Plants get their nutrition from photosynthesis, that is, aborbing energy from the sun's light. There are some carnivorous plants that complements their nutrition eating little insects.
+ 
+ - Callout(Taking good care of a plant):
+    If you follow the last tip, try to not let your plant in the sun's light for too long, or they die! Try to maintain a good balance of time 😊. And don't forget to give water to the soil.
  
  */
